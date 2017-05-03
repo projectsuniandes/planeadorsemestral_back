704d3831b593a17c14068ce9633fc39e32ed78ec
nonameable <ddelgado2013@gmail.com>
2017-05-03 02:29:33 -0500

    merge route done, altough basic courses are not listed

--- app/routes/api/merge.js
+++ app/routes/api/merge.js
@@ -21,12 +21,16 @@ router.route('/')
     // recieve request for merging
     .get(function(req, res) {
         // extract programs from query params and save them in programs
-        let programs = []; //
+
+        let programs = []; //["'FISI'", "'ISIS'"]; //
+        programs.push("'" +req.query.program1 + "'");
+        programs.push("'" +req.query.program2 + "'");
+        programs.push("'" +req.query.program3 + "'");
         //let courses = merger.mergeCourses(programs);
 
-        let sql  = 'SELECT * FROM courses WHERE program_id=1';
-        let answer = "asadasdasd";
 
+        let sql  = "SELECT * FROM courses INNER JOIN programs ON courses.program_id = programs.id WHERE programs.program_code="+ programs[0] + " OR programs.program_code=" + programs[1] + " OR programs.program_code=" + programs[2];
+        console.log(sql);
           query(sql, function(err, result) {
             console.log('entra query');
             if (err){
@@ -37,7 +41,23 @@ router.route('/')
             return res.json(result);
 
           });
-      })
+      });
+
+function getProgramId(programName){
+  let sql  = 'SELECT * FROM courses WHERE program_id=1';
+  let answer = "asadasdasd";
+
+    query(sql, function(err, result) {
+      console.log('entra query');
+      if (err){
+        console.log(err);
+      }
+      console.log(result);
+
+      return res.json(result);
+
+    });
+}
 
 
 module.exports = router;
