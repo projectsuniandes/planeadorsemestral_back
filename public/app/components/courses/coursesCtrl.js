d18d5081d55ad8df122e54bb1e037135eb9a56b2
Juan Guillermo Murillo <jg.murillo10@uniandes.edu.co>
2017-05-08 09:14:00 -0500

    creditos totales

--- public/app/components/courses/coursesCtrl.js
+++ public/app/components/courses/coursesCtrl.js
@@ -14,6 +14,16 @@ angular.module('coursesCtrl', ['coursesService'])
       vm.data = data;
       vm.semesters = vm.data.semesters;
       vm.num = vm.data.numSemesters;
+      vm.totalCredits=0;
+      console.log(vm.data);
+      for (var i = 0;i< vm.data.semesters.length; i++) {
+        vm.data.semesters[i]
+        let semActual= vm.data.semesters[i];
+        for (var j = 0; j < semActual.credits.length; j++) {
+          vm.totalCredits+=semActual.credits[j];
+        }
+      }
+      console.log(vm.totalCredits);
      
     })
   }
